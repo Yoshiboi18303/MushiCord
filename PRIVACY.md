afdf129aa1915c0642beca20965778fe10aafde3
Yoshiboi18303 <yoshiboi18303.t@gmail.com>
2021-08-31 16:28:40 +0000

    Privacy Policy Added

---
+++ PRIVACY.md
@@ -0,0 +1,16 @@
+# MushiCord Privacy Policy
+Here's our Privacy Policy to make sure that we comply with the Discord Developer TOS!
+
+---
+
+1. What do we collect?
+2. How to contact the developer.
+
+---
+1. We do not store any data in any way shape or form.
+
+2. You can contact Yoshiboi18303#4045 in the [support server](https://discord.gg/4TwKaBRvCZ) if you have any concerns.
+
+---
+
+> This may change during updates, please check back every so often.
--- cogs/member.py
+++ cogs/member.py
@@ -23,7 +23,7 @@ class Member_Listeners(commands.Cog):
     
   @commands.Cog.listener()
   async def on_member_remove(self, member):
-    if str(member.guild.id) is not str(882289632673398806):
+    if str(member.guild.id) is not str(882290359827308575):
       return
     members = 0
     for guild in self.bot.guilds: 
@@ -32,7 +32,8 @@ class Member_Listeners(commands.Cog):
     leave = discord.Embed(title="Member Left", description=f"{member.mention} ({member}) left the server on {datetime.date}.", color=0xff0000)
     leave.set_thumbnail(url=member.avatar_url)
     leave.set_footer(text=f"{member.guild.name} now has {true_member_count} members and {members} including bots.")
-    channel = self.bot.get_channel(882059505419026483)
+    channel = self.bot.get_channel(882290375757299733)
+    await member.add_role(882299205740290048)
     await channel.send(embed=leave)
 
 def setup(client):
